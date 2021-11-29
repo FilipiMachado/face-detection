b5aee5f6e06d804683c45328394ce906d17a1281
Filipi <filipisee@hotmail.com>
2021-11-29 15:43:02 -0300

    installed face api dependency

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src="https://images.pexels.com/photos/8356485/pexels-photo-8356485.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+        alt="paisagem"
+        width="940"
+        height="650"
+      />
+    </div>
+  );
+}
+
+export default App;
